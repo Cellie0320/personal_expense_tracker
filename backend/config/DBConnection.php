1e4832d20b9bb0dbef69a129bffd08b0b3cf51e7
Cellie0320 <marceldelange20@gmail.com>
2024-12-14 20:51:29 +0200

    Refactor database connection: replace database.php was changed to DBConnection.php formore clarity and added project structure documentation

--- backend/config/DBConnection.php
+++ backend/config/DBConnection.php
@@ -3,7 +3,8 @@ $dsn = 'mysql:host=localhost;dbname=expensetracker';
 $dbusername = 'root';
 $dbpassword = 'root'; 
 
-// Created a new PDO instance and assigned it to the $pdo variable
+/*Created a new PDO instance and assigned it to the $pdo variable, 
+which allows me to see if I'm connect to my database */
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
     // Set the PDO error mode to exception

76081847e552bae56769bb3f620b11412e4d8419
Cellie0320 <marceldelange20@gmail.com>
2024-12-14 15:58:29 +0200

    created and conected to the my database using database.php

---
+++ backend/config/database.php
@@ -0,0 +1,16 @@
+<?php 
+$dsn = 'mysql:host=localhost;dbname=expensetracker';
+$dbusername = 'root';
+$dbpassword = 'root'; 
+
+// Created a new PDO instance and assigned it to the $pdo variable
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connection successful!';
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
+
